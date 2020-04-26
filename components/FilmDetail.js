0f5dd5ca6ce2d218b15b1892be6f4b0a4436fabe
Mohcine NAZRHAN <nazrhan.mohcine@gmail.com>
2020-04-26 21:17:55 +0000

    feat: add share btn for android and ios

--- components/FilmDetail.js
+++ components/FilmDetail.js
@@ -1,59 +1,147 @@
-import React from 'react'
-import { StyleSheet, View, Text, ActivityIndicator, ScrollView, Image, TouchableOpacity } from 'react-native'
-import { getFilmDetailFromApi, getImageFromApi } from '../API/TMDBApi'
-import moment from 'moment'
-import numeral from 'numeral'
-import { connect } from 'react-redux'
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ActivityIndicator,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Share,
+  Platform,
+} from "react-native";
+import { getFilmDetailFromApi, getImageFromApi } from "../API/TMDBApi";
+import moment from "moment";
+import numeral from "numeral";
+import { connect } from "react-redux";
 
 class FilmDetail extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    const { params } = navigation.state;
+    // On accède à la fonction shareFilm et au film via les paramètres qu'on a ajouté à la navigation
+    if (params.film != undefined && Platform.OS === "ios") {
+      return {
+        // On a besoin d'afficher une image, il faut donc passe par une Touchable une fois de plus
+        headerRight: (
+          <TouchableOpacity
+            style={styles.share_touchable_headerrightbutton}
+            onPress={() => params.shareFilm()}
+          >
+            <Image
+              style={styles.share_image}
+              source={require("../Images/ic_share.png")}
+            />
+          </TouchableOpacity>
+        ),
+      };
+    }
+  };
+
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       film: undefined,
-      isLoading: true
-    }
+      isLoading: true,
+    };
+    // Ne pas oublier de binder la fonction _shareFilm sinon, lorsqu'on va l'appeler depuis le headerRight de la navigation, this.state.film sera undefined et fera planter l'application
+    this._shareFilm = this._shareFilm.bind(this);
   }
 
+  // Fonction pour faire passer la fonction _shareFilm et le film aux paramètres de la navigation. Ainsi on aura accès à ces données au moment de définir le headerRight
+  _updateNavigationParams() {
+    this.props.navigation.setParams({
+      shareFilm: this._shareFilm,
+      film: this.state.film,
+    });
+  }
+
+  // Dès que le film est chargé, on met à jour les paramètres de la navigation (avec la fonction _updateNavigationParams) pour afficher le bouton de partage
   componentDidMount() {
-    getFilmDetailFromApi(this.props.route.params.idFilm).then(data => {
-      this.setState({
-        film: data,
-        isLoading: false
-      })
-    })
+    const favoriteFilmIndex = this.props.favoritesFilm.findIndex(
+      (item) => item.id === this.props.route.params.idFilm
+    );
+    if (favoriteFilmIndex !== -1) {
+      this.setState(
+        {
+          film: this.props.favoritesFilm[favoriteFilmIndex],
+        },
+        () => {
+          this._updateNavigationParams();
+        }
+      );
+      this.setState({ isLoading: false });
+      return;
+    }
+
+    this.setState({ isLoading: true });
+    getFilmDetailFromApi(this.props.route.params.idFilm).then(
+      (data) => {
+        this.setState(
+          {
+            film: data,
+            isLoading: false,
+          },
+          () => {
+            this._updateNavigationParams();
+          }
+        );
+      }
+    );
+  }
+
+  _shareFilm() {
+    const { film } = this.state;
+    Share.share({ title: film.title, message: film.overview });
+  }
+
+  _displayFloatingActionButton() {
+    const { film } = this.state;
+    if (film != undefined && Platform.OS === "android") {
+      // Uniquement sur Android et lorsque le film est chargé
+      return (
+        <TouchableOpacity
+          style={styles.share_touchable_floatingactionbutton}
+          onPress={() => this._shareFilm()}
+        >
+          <Image
+            style={styles.share_image}
+            source={require("../Images/ic_share.png")}
+          />
+        </TouchableOpacity>
+      );
+    }
   }
 
   _displayLoading() {
     if (this.state.isLoading) {
       return (
         <View style={styles.loading_container}>
-          <ActivityIndicator size='large' />
+          <ActivityIndicator size="large" />
         </View>
-      )
+      );
     }
   }
 
   _toggleFavorite() {
-    const action = { type: "TOGGLE_FAVORITE", value: this.state.film }
-    this.props.dispatch(action)
+    const action = { type: "TOGGLE_FAVORITE", value: this.state.film };
+    this.props.dispatch(action);
   }
 
   _displayFavoriteImage() {
-    var sourceImage = require('../Images/ic_favorite_border.png')
-    if (this.props.favoritesFilm.findIndex(item => item.id === this.state.film.id) !== -1) {
+    var sourceImage = require("../Images/ic_favorite_border.png");
+    if (
+      this.props.favoritesFilm.findIndex(
+        (item) => item.id === this.state.film.id
+      ) !== -1
+    ) {
       // Film dans nos favoris
-      sourceImage = require('../Images/ic_favorite.png')
+      sourceImage = require("../Images/ic_favorite.png");
     }
-    return (
-      <Image
-        style={styles.favorite_image}
-        source={sourceImage}
-      />
-    )
+    return <Image style={styles.favorite_image} source={sourceImage} />;
   }
 
   _displayFilm() {
-    const { film } = this.state
+    const { film } = this.state;
     if (film != undefined) {
       return (
         <ScrollView style={styles.scrollview_container}>
@@ -64,24 +152,41 @@ class FilmDetail extends React.Component {
           <Text style={styles.title_text}>{film.title}</Text>
           <TouchableOpacity
             style={styles.favorite_container}
-            onPress={() => this._toggleFavorite()}>
+            onPress={() => this._toggleFavorite()}
+          >
             {this._displayFavoriteImage()}
           </TouchableOpacity>
           <Text style={styles.description_text}>{film.overview}</Text>
-          <Text style={styles.default_text}>Sorti le {moment(new Date(film.release_date)).format('DD/MM/YYYY')}</Text>
-          <Text style={styles.default_text}>Note : {film.vote_average} / 10</Text>
-          <Text style={styles.default_text}>Nombre de votes : {film.vote_count}</Text>
-          <Text style={styles.default_text}>Budget : {numeral(film.budget).format('0,0[.]00 $')}</Text>
-          <Text style={styles.default_text}>Genre(s) : {film.genres.map(function (genre) {
-            return genre.name;
-          }).join(" / ")}
+          <Text style={styles.default_text}>
+            Sorti le {moment(new Date(film.release_date)).format("DD/MM/YYYY")}
           </Text>
-          <Text style={styles.default_text}>Companie(s) : {film.production_companies.map(function (company) {
-            return company.name;
-          }).join(" / ")}
+          <Text style={styles.default_text}>
+            Note : {film.vote_average} / 10
+          </Text>
+          <Text style={styles.default_text}>
+            Nombre de votes : {film.vote_count}
+          </Text>
+          <Text style={styles.default_text}>
+            Budget : {numeral(film.budget).format("0,0[.]00 $")}
+          </Text>
+          <Text style={styles.default_text}>
+            Genre(s) :{" "}
+            {film.genres
+              .map(function (genre) {
+                return genre.name;
+              })
+              .join(" / ")}
+          </Text>
+          <Text style={styles.default_text}>
+            Companie(s) :{" "}
+            {film.production_companies
+              .map(function (company) {
+                return company.name;
+              })
+              .join(" / ")}
           </Text>
         </ScrollView>
-      )
+      );
     }
   }
 
@@ -90,48 +195,49 @@ class FilmDetail extends React.Component {
       <View style={styles.main_container}>
         {this._displayLoading()}
         {this._displayFilm()}
+        {this._displayFloatingActionButton()}
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   main_container: {
-    flex: 1
+    flex: 1,
   },
   loading_container: {
-    position: 'absolute',
+    position: "absolute",
     left: 0,
     right: 0,
     top: 0,
     bottom: 0,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
   scrollview_container: {
-    flex: 1
+    flex: 1,
   },
   image: {
     height: 169,
-    margin: 5
+    margin: 5,
   },
   title_text: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 35,
     flex: 1,
-    flexWrap: 'wrap',
+    flexWrap: "wrap",
     marginLeft: 5,
     marginRight: 5,
     marginTop: 10,
     marginBottom: 10,
-    color: '#000000',
-    textAlign: 'center'
+    color: "#000000",
+    textAlign: "center",
   },
   description_text: {
-    fontStyle: 'italic',
-    color: '#666666',
+    fontStyle: "italic",
+    color: "#666666",
     margin: 5,
-    marginBottom: 15
+    marginBottom: 15,
   },
   default_text: {
     marginLeft: 5,
@@ -139,24 +245,44 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
   favorite_container: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   favorite_image: {
     width: 40,
-    height: 40
-  }
-})
+    height: 40,
+  },
+  share_touchable_floatingactionbutton: {
+    position: "absolute",
+    width: 60,
+    height: 60,
+    right: 30,
+    bottom: 30,
+    borderRadius: 30,
+    backgroundColor: "#e91e63",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  share_image: {
+    width: 30,
+    height: 30,
+  },
+  share_touchable_headerrightbutton: {
+    marginRight: 8,
+  },
+});
 
 const mapStateToProps = (state) => {
   return {
-    favoritesFilm: state.favoritesFilm
-  }
-}
+    favoritesFilm: state.favoritesFilm,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    dispatch: (action) => { dispatch(action) }
-  }
-}
+    dispatch: (action) => {
+      dispatch(action);
+    },
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(FilmDetail)
+export default connect(mapStateToProps, mapDispatchToProps)(FilmDetail);
