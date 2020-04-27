21eae47767c7bde768c02bdf101c2af62b22c089
Mohcine NAZRHAN <nazrhan.mohcine@gmail.com>
2020-04-27 21:32:53 +0000

    feat: add film list container to make list centred

--- components/Favorites.js
+++ components/Favorites.js
@@ -1,27 +1,33 @@
-import React from 'react'
-import { StyleSheet, Text } from 'react-native'
-import FilmList from './FilmList'
-import { connect } from 'react-redux'
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import FilmList from "./FilmList";
+import { connect } from "react-redux";
 
 class Favorites extends React.Component {
-
   render() {
     return (
-      <FilmList
-        films={this.props.favoritesFilm}
-        navigation={this.props.navigation}
-        favoriteList={true}
-      />
-    )
+      <View style={styles.filmListContainer}>
+        <FilmList
+          films={this.props.favoritesFilm}
+          navigation={this.props.navigation}
+          favoriteList={true}
+        />
+      </View>
+    );
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  filmListContainer: {
+    flex: 1,
+    margin: 5,
+  },
+});
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    favoritesFilm: state.favoritesFilm
-  }
-}
+    favoritesFilm: state.favoritesFilm,
+  };
+};
 
-export default connect(mapStateToProps)(Favorites)
+export default connect(mapStateToProps)(Favorites);
