1b41f6f5aabda6915b3d2a43b1815656d28d84b8
Mohcine NAZRHAN <nazrhan.mohcine@gmail.com>
2020-05-04 23:08:22 +0000

    refactor: add base url var to avoid duplication

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,7 +1,8 @@
-import { API_TOKEN } from 'react-native-dotenv'
+import { API_TOKEN } from 'react-native-dotenv';
+const baseUrl = 'https://api.themoviedb.org/3';
 
 export function getFilmsFromApiWithSearchedText(text, page) {
-  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+  const url = baseUrl + '/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
   return fetch(url)
     .then((response) => response.json())
     .catch((error) => console.error(error))
@@ -12,7 +13,7 @@ export function getImageFromApi(name) {
 }
 
 export function getFilmDetailFromApi(id) {
-  return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+  return fetch(baseUrl + '/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
     .then((response) => response.json())
     .catch((error) => console.error(error));
 }
