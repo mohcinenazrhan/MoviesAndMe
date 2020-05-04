b98d7f82e3ec484c33734d8166e64e1231a2f54e
Mohcine NAZRHAN <nazrhan.mohcine@gmail.com>
2020-05-04 23:23:41 +0000

    refactor: add lang var to make changable alter on

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,8 +1,9 @@
 import { API_TOKEN } from 'react-native-dotenv';
 const baseUrl = 'https://api.themoviedb.org/3';
+const lang = 'fr';
 
 export function getFilmsFromApiWithSearchedText(text, page) {
-  const url = baseUrl + '/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
+  const url = baseUrl + '/search/movie?api_key=' + API_TOKEN + `&language=${lang}&query=` + text + "&page=" + page
   return fetch(url)
     .then((response) => response.json())
     .catch((error) => console.error(error))
@@ -13,7 +14,7 @@ export function getImageFromApi(name) {
 }
 
 export function getFilmDetailFromApi(id) {
-  return fetch(baseUrl + '/movie/' + id + '?api_key=' + API_TOKEN + '&language=fr')
+  return fetch(baseUrl + '/movie/' + id + '?api_key=' + API_TOKEN + `&language=${lang}`)
     .then((response) => response.json())
     .catch((error) => console.error(error));
 }
